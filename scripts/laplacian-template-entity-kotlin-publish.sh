8e556bba815994afd634e10377188f656fd598c0
iwauo <iwauo.tajima@gmail.com>
2020-03-27 18:45:59 +0900

    update the project's info

--- scripts/laplacian-template-entity-kotlin-publish.sh
+++ scripts/laplacian-template-entity-kotlin-publish.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
 set -e
 SCRIPT_BASE_DIR=$(cd $"${BASH_SOURCE%/*}" && pwd)
 PROJECT_BASE_DIR=$(cd $SCRIPT_BASE_DIR && cd .. && pwd)
 
-TARGET_PROJECT_DIR=./generated-projects/laplacian.template.entity.kotlin
+TARGET_PROJECT_DIR="${PROJECT_BASE_DIR}/./generated-projects/laplacian.template.entity.kotlin"
 
 (cd $TARGET_PROJECT_DIR
   if [[ ! -f ./scripts/laplacian-generate.sh ]]
