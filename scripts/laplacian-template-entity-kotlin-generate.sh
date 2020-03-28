ab3066c09d1df0ce9d925180fc7189e8abc3636f
iwauo <iwauo.tajima@gmail.com>
2020-03-28 09:36:53 +0900

    update script

--- scripts/laplacian-template-entity-kotlin-generate.sh
+++ scripts/laplacian-template-entity-kotlin-generate.sh
@@ -7,22 +7,28 @@ TARGET_PROJECT_DIR=./generated-projects/laplacian.template.entity.kotlin
 TARGET_MODEL_DIR="$TARGET_PROJECT_DIR/model"
 TARGET_PROJECT_MODEL_FILE="$TARGET_MODEL_DIR/template-entity-kotlin-project.yaml"
 
-normalize_path () {
-  local path=$1
-  if [[ $path == /* ]]
-  then
-    echo $path
-  else
-    echo "${PROJECT_BASE_DIR}/$path"
-  fi
+GENERATOR_SCRIPT_FILE_NAME=laplacian-template-entity-kotlin-generate.sh
+TARGET_SCRIPT_DIR="$TARGET_PROJECT_DIR/scripts"
+TARGET_PROJECT_GENERATOR_SCRIPT="$TARGET_SCRIPT_DIR/$GENERATOR_SCRIPT_FILE_NAME"
+
+main() {
+  set -x
+  checkout_from_code_repository
+  create_project_model_file
+  run_generator
 }
 
-mkdir -p $TARGET_MODEL_DIR
-cat <<END_FILE > $TARGET_PROJECT_MODEL_FILE
+create_project_model_file() {
+  mkdir -p $TARGET_MODEL_DIR
+  cat <<END_FILE > $TARGET_PROJECT_MODEL_FILE
 project:
   group: laplacian
   name: template.entity.kotlin
   namespace: laplacian
+  version: '1.0.0'
+  source_repository:
+    url: https://github.com/nabla-squared/laplacian.template.entity.kotlin.git
+    branch: master
   subprojects: []
   schemas: []
   templates:
@@ -33,17 +39,45 @@ project:
   template_files:
   - $(normalize_path './laplacian.template.project.gradle/template')
 END_FILE
+}
 
-GENERATOR_SCRIPT_FILE_NAME=laplacian-template-entity-kotlin-generate.sh
-TARGET_SCRIPT_DIR="$TARGET_PROJECT_DIR/scripts"
-TARGET_PROJECT_GENERATOR_SCRIPT="$TARGET_SCRIPT_DIR/$GENERATOR_SCRIPT_FILE_NAME"
-
-mkdir -p $TARGET_SCRIPT_DIR
-
-(cd $TARGET_PROJECT_DIR
-  if [[ ! -f ./scripts/laplacian-generate.sh ]]
+checkout_from_code_repository() {
+  if [[ ! -d $TARGET_PROJECT_DIR/.git ]]
   then
-    curl -Ls https://git.io/fhxcl | bash
+    if ! (git remote | grep -Fx 'template-entity-kotlin')
+    then
+      git remote add \
+        -f template-entity-kotlin \
+        https://github.com/nabla-squared/laplacian.template.entity.kotlin.git
+    fi
+
+    git subtree add \
+      --prefix ./generated-projects/laplacian.template.entity.kotlin \
+      template-entity-kotlin \
+      master \
+      --squash
   fi
-  ./scripts/laplacian-project-generate.sh
-)
+}
+
+run_generator() {
+  mkdir -p $TARGET_SCRIPT_DIR
+  (cd $TARGET_PROJECT_DIR
+    if [[ ! -f ./scripts/laplacian-generate.sh ]]
+    then
+      curl -Ls https://git.io/fhxcl | bash
+    fi
+    ./scripts/laplacian-project-generate.sh
+  )
+}
+
+normalize_path () {
+  local path=$1
+  if [[ $path == /* ]]
+  then
+    echo $path
+  else
+    echo "${PROJECT_BASE_DIR}/$path"
+  fi
+}
+
+main
